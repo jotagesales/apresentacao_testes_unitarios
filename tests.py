e8c7020aeb981e1af63cf4b7fbac42419214aabf
Jotagê Sales <jotage_sales@hotmail.com>
2015-08-18 21:06:44 -0300

    initial commit

---
+++ tests.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+Created by Jotagê Sales on 18/08/15.
+'''
+
+import unittest
+from vogais import conta_vogais_consoantes
+
+class TestVogais(unittest.TestCase):
+
+    def test_conta_vogais(self):
+        qtde_vogais, qtde_consoantes = conta_vogais_consoantes('Python')
+
+        self.assertEqual(qtde_vogais, 1)
+        self.assertEqual(qtde_consoantes, 5)
+
+unittest.main()
---
+++ vogais.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+Created by Jotagê Sales on 18/08/15.
+'''
+
+VOGAIS = ['a', 'e', 'i', 'o', 'u']
+
+
+def conta_vogais_consoantes(text=''):
+    count_vogal= 0
+    count_consoante= 0
+
+    for letra in text:
+        # deixando minusculo e removendo os espaços
+        letra = letra.lower().strip()
+
+        if letra:
+            if letra in VOGAIS:
+                count_vogal += 1
+            else:
+                count_consoante += 1
+
+    return count_vogal, count_consoante
